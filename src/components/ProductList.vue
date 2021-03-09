ff817e360bd857084a3b56fe12310672a6145fa9
jolyfun <73360217+jolyfun@users.noreply.github.com>
2021-03-09 21:36:25 +0300

    Add files via upload

---
+++ src/components/ProductList.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul class="catalog__list">
+
+      <ProductItem v-for="(product, index) in products" :key="index" :product='products'></ProductItem>
+
+    </ul>
+</template>
+
+<script>
+  import ProductItem from '@/ProductItem'
+
+  export default {
+    components: {ProductItem: ProductItem},
+    props: ['products']
+  }
+</script>
