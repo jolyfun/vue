2217837f2a4e0653b3a0d66d36885d3a80ae4ec6
jolyfun <73360217+jolyfun@users.noreply.github.com>
2021-03-12 01:46:56 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+ 
+      <section class="catalog">
+        
+        <ProductList :products='products'/>
+
+        <BasePagination v-model='page' :count='countProducts' :perPage='productListPerPage'/>
+
+      </section>
+  
+</template>
+
+<script>
+import products from '@/data/products'
+import ProductList from '@/components/ProductList.vue'
+import BasePagination from '@/components/BasePagination'
+
+export default {
+  name: 'App',
+  components: {ProductList, BasePagination},
+  data() {
+    return {
+      // products
+      page: 1,
+      productListPerPage: 3,
+    }
+  },
+  computed: {
+    products() {
+      const offset = (this.page - 1) * this.productListPerPage;
+      return products.slice(offset, offset + this.productListPerPage)
+    },
+    countProducts() {
+      return products.length
+    }
+  }
+}
+</script>
+
+
